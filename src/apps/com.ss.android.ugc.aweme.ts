8e8ae4b4f3e9fa4439362ce451844d9c7f0519e4
cjy0812 <cjy081219@Gmail.com>
2026-02-07 23:16:13 +0800

    feat: 全屏广告-话题视频推荐

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -19,5 +19,20 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-话题视频推荐',
+      desc: '使用返回来跳过话题视频',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.ss.android.ugc.aweme.main.MainActivity',
+          action: 'back',
+          matches:
+            '[desc="不感兴趣"][clickable=false] +3 @[desc="立即参与"][clickable=false][visibleToUser=true] +3 [desc="上滑继续看视频"][clickable=false] <<426 [vid="viewpager"][desc="视频"]',
+          snapshotUrls: 'https://i.gkd.li/i/25025682',
+        },
+      ],
+    },
   ],
 });
