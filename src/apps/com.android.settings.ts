adfe5ab4c77040b111f16498b07bdc6f12786c3e
cjy0812 <cjy081219@Gmail.com>
2026-02-24 22:38:27 +0800

    feat: 设置 局部广告-电子保修卡

---
+++ src/apps/com.android.settings.ts
@@ -0,0 +1,22 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.android.settings',
+  name: '设置',
+  groups: [
+    {
+      key: 1,
+      name: '局部广告-电子保修卡',
+      desc: '(VIVO📱)首页-x掉',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.android.settings.homepage.SettingsHomepageActivity',
+          matches:
+            '[vid="dashboard_tile"] > [vid="delete"][clickable=true][visibleToUser=true]',
+          snapshotUrls: 'https://li.chenge.eu.org/i/25522123',
+        },
+      ],
+    },
+  ],
+});
