5aabfab6929ce6399483d6a5b0ef260e906ba4f9
cjy0812 <cjy081219@Gmail.com>
2026-05-20 00:37:51 +0800

    feat: Wechat 功能类-付款后自动点击完成/返回商家

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -24,5 +24,37 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '功能类-付款后自动点击完成/返回商家',
+      desc: '6s后点击完成/返回商家按钮',
+      rules: [
+        {
+          key: 0,
+          fastQuery: true,
+          activityIds: '.framework.app.UIPageFragmentActivity',
+          actionDelay: 6000,
+          matches:
+            '[vid="kinda_button_impl_wrapper"][desc="完成" || desc="返回商家"]',
+          exampleUrls:
+            'https://m.gkd.li/57941037/a5177d9d-6745-443f-baf5-af57153430d8',
+          snapshotUrls: [
+            'https://i.gkd.li/i/14399355',
+            'https://i.gkd.li/i/14662147',
+            'https://i.gkd.li/i/24157874',
+          ],
+          excludeSnapshotUrls: 'https://i.gkd.li/i/14532946',
+        },
+        {
+          key: 1,
+          versionCode: { minimum: 2980 },
+          activityIds: '.plugin.lite.ui.WxaLiteAppTransparentLiteUI',
+          actionDelay: 6000,
+          matches: 'Button[desc="返回商家"][visibleToUser=true]',
+          exampleUrls: 'https://e.gkd.li/e7acbb38-c602-4ea4-88ec-7604c3aeae06',
+          snapshotUrls: 'https://i.gkd.li/i/24141842',
+        },
+      ],
+    },
   ],
 });
