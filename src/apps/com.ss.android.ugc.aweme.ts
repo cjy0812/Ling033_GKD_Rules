f835c777cdc418e3efb35f2c83a5794c7f7c5a8a
Ling0402 <cjy081219@Gmail.com>
2026-06-29 03:16:04 +0800

    del: 抖音 全屏广告-创作者广告分成
    重复

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -5,21 +5,6 @@ export default defineGkdApp({
   name: '抖音',
   groups: [
     {
-      key: 1,
-      name: '全屏广告-创作者广告分成',
-      desc: '刷视频出现,使用返回来跳过',
-      rules: [
-        {
-          fastQuery: true,
-          activityIds: 'com.ss.android.ugc.aweme.detail.ui.DetailActivity',
-          action: 'back',
-          matches:
-            '[text="激励更好创作"][visibleToUser=true][childCount=0] <<62 LinearLayout [text$="广告"][visibleToUser=true][childCount=0]',
-          snapshotUrls: 'https://i.gkd.li/i/25025779',
-        },
-      ],
-    },
-    {
       key: 2,
       name: '全屏广告-话题视频推荐',
       desc: '使用返回来跳过话题视频',
